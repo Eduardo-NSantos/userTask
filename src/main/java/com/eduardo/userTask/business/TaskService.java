09a6fbb1e162ee789cc29b3460c0f690ebda3a7c
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-04-11 19:30:15 -0300

    adicão dos metodos create e findAll

--- src/main/java/com/eduardo/userTask/business/TaskService.java
+++ src/main/java/com/eduardo/userTask/business/TaskService.java
@@ -1,4 +1,42 @@
 package com.eduardo.userTask.business;
 
+import com.eduardo.userTask.dto.TaskDTO.TaskRequestDTO;
+import com.eduardo.userTask.dto.TaskDTO.TaskResponseDTO;
+import com.eduardo.userTask.enums.TaskStatus;
+import com.eduardo.userTask.infrastructure.entities.Task;
+import com.eduardo.userTask.infrastructure.entities.User;
+import com.eduardo.userTask.infrastructure.repositories.TaskRepository;
+import com.eduardo.userTask.mapper.TaskMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+    private final TaskRepository repository;
+    private final TaskMapper mapper;
+    private final UserService userService;
+
+    public TaskResponseDTO create(Integer userId, TaskRequestDTO dto){
+        User user = userService.findEntity(userId);
+
+        Task task = mapper.toEntity(dto);
+        task.setUser(user);
+
+        task.setStatus(
+                dto.getStatus() != null ? dto.getStatus() : TaskStatus.PENDING
+        );
+
+        Task saved = repository.save(task);
+        return mapper.toDTO(saved);
+    }
+
+    public List<TaskResponseDTO> findAll(){
+        return repository.findAll()
+                .stream()
+                .map(mapper::toDTO)
+                .toList();
+    }
 }
