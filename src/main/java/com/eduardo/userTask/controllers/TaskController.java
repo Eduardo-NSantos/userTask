6271e89bc868f9204bfb67a68989346a37fe7a2f
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-04-11 19:31:23 -0300

    adição dos metodos create e getAll

--- src/main/java/com/eduardo/userTask/controllers/TaskController.java
+++ src/main/java/com/eduardo/userTask/controllers/TaskController.java
@@ -1,4 +1,32 @@
 package com.eduardo.userTask.controllers;
 
+import com.eduardo.userTask.business.TaskService;
+import com.eduardo.userTask.dto.TaskDTO.TaskRequestDTO;
+import com.eduardo.userTask.dto.TaskDTO.TaskResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+    private final TaskService task;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> create(
+            @PathVariable Integer id,
+            @RequestBody TaskRequestDTO dto
+    ){
+        TaskResponseDTO saved = task.create(id, dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task.create(id, dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDTO>> getAll(){
+        return ResponseEntity.ok(task.findAll());
+    }
 }
