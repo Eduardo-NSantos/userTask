36a0d733ce03b54519e7fa1a13863931cafe640a
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-04-30 12:00:44 -0300

    criação de rotas para autenticação

--- src/main/java/com/eduardo/userTask/business/AuthenticationService.java
+++ src/main/java/com/eduardo/userTask/business/AuthenticationService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService {
+public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final AuthenticationMapper mapper;
     private final UserService userService;
--- src/main/java/com/eduardo/userTask/controllers/AuthenticationController.java
+++ src/main/java/com/eduardo/userTask/controllers/AuthenticationController.java
@@ -1,4 +1,32 @@
 package com.eduardo.userTask.controllers;
 
+import com.eduardo.userTask.business.AuthenticationService;
+import com.eduardo.userTask.dto.authenticationDTO.LoginDTO;
+import com.eduardo.userTask.dto.authenticationDTO.RegisterDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+    private final AuthenticationService auth;
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody @Valid LoginDTO login){
+        auth.login(login);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid RegisterDTO register){
+        auth.register(register);
+        return ResponseEntity.ok().build();
+    }
 }
